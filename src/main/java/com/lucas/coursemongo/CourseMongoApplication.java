bd80fdb2852642a20a5a4b929bf2e983b1360d2d
Lucas Rodrigues <contato.lucasrodrigues99@gmail.com>
2020-01-07 14:43:49 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/lucas/coursemongo/CourseMongoApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.coursemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/coursemongo/CourseMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.coursemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
