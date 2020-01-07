2faba17c8e56da7daa0c99ea1fdc57de1c6bba89
Lucas Rodrigues <contato.lucasrodrigues99@gmail.com>
2020-01-07 19:33:10 -0300

    Consulta com vários critérios

--- src/main/java/com/lucas/coursemongo/config/Instantiation.java
+++ src/main/java/com/lucas/coursemongo/config/Instantiation.java
@@ -39,8 +39,8 @@ public class Instantiation implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("2018/03/21"), "Partiu viagem",  "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("2018/03/23"), "Bom dia",  "Acordei feliz hoje!", new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem",  "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia",  "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		
 		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("2018/03/21"), new AuthorDTO(alex));
