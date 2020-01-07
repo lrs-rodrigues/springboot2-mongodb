0f72b70f30ae76fe6e2408279e4846cbdbf7e987
Lucas Rodrigues <contato.lucasrodrigues99@gmail.com>
2020-01-07 17:54:49 -0300

    Obtendo um post por id

---
+++ src/main/java/com/lucas/coursemongo/resources/PostResource.java
@@ -0,0 +1,34 @@
+package com.lucas.coursemongo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lucas.coursemongo.domain.Post;
+import com.lucas.coursemongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findAll() {
+		List<Post> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
---
+++ src/main/java/com/lucas/coursemongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.lucas.coursemongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucas.coursemongo.domain.Post;
+import com.lucas.coursemongo.repositories.PostRepository;
+import com.lucas.coursemongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
